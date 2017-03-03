5e6518013f933bed30eb02626515deb90565ca11
Krister <krister.torsvik@gmail.com>
2017-03-03 16:04:31 +0100

    top layout done

--- src/+app/app.component.ts
+++ src/+app/app.component.ts
@@ -24,36 +24,161 @@ export class XLargeDirective {
   encapsulation: ViewEncapsulation.Emulated,
   selector: 'app',
   styles: [`
-    * { padding:0; margin:0; font-family: 'Droid Sans', sans-serif; }
-    #universal { text-align:center; font-weight:bold; padding:15px 0; }
-    nav { background:#158126; min-height:40px; border-bottom:5px #046923 solid; }
-    nav a { font-weight:bold; text-decoration:none; color:#fff; padding:20px; display:inline-block; }
-    nav a:hover { background:#00AF36; }
-    .hero-universal { min-height:500px; display:block; padding:20px; background: url('/assets/logo.png') no-repeat center center; }
-    .inner-hero { background: rgba(255, 255, 255, 0.75); border:5px #ccc solid; padding:25px; }
-    .router-link-active { background-color: #00AF36; }
-    main { padding:20px 0; }
-    pre { font-size:12px; }
-    .background-image { display: flex; }
+  * {
+    padding:0;
+    margin:0;
+    font-family: 'Droid Sans', sans-serif;
+  }
+
+  #universal {
+    text-align:center;
+    font-weight:bold;
+    padding:15px 0;
+  }
+
+  nav {
+    position: fixed;
+    right: 0;
+    left: 0;
+    z-index: 999;
+    
+    display: flex;
+    background: LightSkyBlue;
+    opacity: 0.8;
+    min-height:40px;
+    align-items: center;
+    justify-content: center;
+  }
+
+  nav a {
+    font-weight:bold;
+    text-decoration:none;
+    color:#fff;
+    padding: 20px;
+    display:inline-block;
+  }
+
+  nav a:hover {
+    background: DodgerBlue;
+  }
+
+  .hero-universal {
+    min-height:500px;
+    display:block;
+    padding:20px;
+  }
+
+  main {
+    padding:20px 0;
+  }
+
+  pre {
+    font-size:12px;
+  }
+
+  .logo {
+      width: 60px;
+      height: 20px;
+    }
+
+  .header {
+    width: 100vw;
+    height: 80vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    background-image: url('./assets/images/bakgrunn.jpg');
+    background-size: cover; 
+    background-repeat: no-repeat;
+    background-attatchment: fixed;
+  }
+
+  h3 {
+    font-size: 2rem;
+    padding: 5rem;
+    text-shadow: 1px 1px #333;
+    color: #fff;
+  }
+
+  /* Dropdown Button */
+.dropbtn {
+    background-color: LightSkyBlue;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: LightSkyBlue;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+    color: white;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: DodgerBlue;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {
+    background-color: LightSkyBlue;
+    opacity: 0.8;
+}
   `],
-  template: `
-  <div class="background-image">
-    <img src="./assets/images/bakgrunn.jpg" alt="Bakgrunnsbilde">
-  </div>
-  <h3 id="universal">Jølvassbu Camping</h3>
-  <nav>
-    <a routerLinkActive="router-link-active" routerLink="home">Home</a>
-    <a routerLinkActive="router-link-active" routerLink="about">About</a>
-    <a routerLinkActive="router-link-active" routerLink="todo">Todo</a>
-    <a routerLinkActive="router-link-active" routerLink="lazy">Lazy</a>
-    <a routerLinkActive="router-link-active" routerLink="cabins">Cabins</a>
-  </nav>
-  <div class="hero-universal">
-      <main>
-        <router-outlet></router-outlet>
-      </main>
-  </div>
+  template:`
+    <nav>
+      <a routerLinkActive="router-link-active" routerLink="home">
+        <img class="logo" src="./assets/images/logo-jølvassbu.gif">
+      </a>
+      <div class="dropdown">
+        <button class="dropbtn">Overnatting</button>
+        <div class="dropdown-content">
+          <a routerLinkActive="router-link-active" routerLink="cabins">Cabins</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">Aktiviteter</button>
+        <div class="dropdown-content">
+          <a routerLinkActive="router-link-active" routerLink="about">About</a>
+          <a routerLinkActive="router-link-active" routerLink="todo">Todo</a>
+          <a routerLinkActive="router-link-active" routerLink="lazy">Lazy</a>
+        </div>
+      </div>
+    </nav>
+    <div class="header">
+      <h3 id="universal">Jølvassbu Camping</h3>
+    </div>
+    <div class="hero-universal">
+        <main>
+          <router-outlet></router-outlet>
+        </main>
+    </div>
   `
 })
-export class AppComponent {
-}
+export class AppComponent {}
