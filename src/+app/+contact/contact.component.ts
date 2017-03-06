9828a4f7f9ce4d6c44e26fce5dde8d7b3245bafd
Krister <krister.torsvik@gmail.com>
2017-03-06 15:03:54 +0100

    removed unused files and readying content

--- src/+app/+contact/contact.component.ts
+++ src/+app/+contact/contact.component.ts
@@ -1,4 +1,9 @@
 import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/Rx';
+
+declare var emailjs:any;
 
 @Component({
   changeDetection: ChangeDetectionStrategy.Default,
@@ -9,5 +14,23 @@ import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/
 })
 export class ContactComponent {
 
-   public constructor() {}
+  public constructor(private http: Http) {}
+
+      send(form: NgForm) {
+        const subject = form.value.subject;
+        const text = form.value.message;
+        const service_id = "default_service";
+        const template_id = "template_Ass4sDXO";
+        const params = {
+          subject: subject,
+          message: text
+        }
+        emailjs.send(service_id, template_id, params)
+          .then(function(){
+             alert("Sent!");
+           }, function(err) {
+             alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+          });
+         form.reset();
+       }
 }
